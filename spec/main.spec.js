cb97318151787db45e84fb25f27579ed952b7434
Guillaume Grossetie <ggrossetie@gmail.com>
2016-07-18 17:45:41 +0200

    Prepare version 0.10.1
     - Upgrade to Opal 0.10.1
     - Adds tests (Jasmine)

---
+++ spec/main.spec.js
@@ -0,0 +1,25 @@
+var Opal = require('../src/index').Opal;
+
+describe('Opal node runtime', function () {
+
+  describe('When loaded', function() {
+    it('Opal should not be null', function() {
+      expect(Opal).not.toBe(null);
+    });
+
+    it('Pathname methods should be available', function() {
+      Opal.load('pathname');
+      var Pathname = Opal.get('Pathname');
+      var path1 = Pathname.$new('/foo/bar');
+      var path2 = Pathname.$new('qux');
+      expect(path1['$+'](path2)['$to_path']()).toBe('/foo/bar/qux');
+    });
+
+    it('Node.js specific implementations should be available', function() {
+      Opal.load('nodejs');
+      var Dir = Opal.get('Dir');
+      var currentDir = Dir['$pwd']();
+      expect(currentDir).toBe(process.cwd());
+    });
+  });
+});
