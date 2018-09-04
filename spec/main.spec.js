310b98d2a95dcd3fbe90550c1e61c1e1c060cf81
Guillaume Grossetie <ggrossetie@gmail.com>
2018-09-04 13:41:04 +0200

    Upgrade development dependencies
    - Node.js 8.11+
    - Remove yarn.lock (switch to npm)
    - Replace Jasmine by Mocha+chai

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,35 +1,38 @@
-var Opal = require('../src/index').Opal;
+const chai = require('chai');
+const expect = chai.expect;
+
+const Opal = require('../src/index').Opal;
 
 describe('Opal Node Runtime', function () {
 
   describe('When loaded', function() {
-    it('should be instantiated', function() {
-      expect(Opal).not.toBe(null);
+    it('should export Opal object', function() {
+      expect(Opal).not.be.null;
     });
 
     it('should preserve Function methods', function() {
-      expect(Function.call).toBeDefined();
-      expect(Function.apply).toBeDefined();
-      expect(Function.bind).toBeDefined();
+      expect(Function.call).to.be.an.instanceof(Function);
+      expect(Function.apply).to.be.an.instanceof(Function);
+      expect(Function.bind).to.be.an.instanceof(Function);
     });
   });
 
   describe('When pathname module is loaded', function() {
-    it('should resolve Pathname methods', function() {
+    it('should register Pathname methods', function() {
       Opal.load('pathname');
       var Pathname = Opal.const_get_relative([], 'Pathname');
       var path1 = Pathname.$new('/foo/bar');
       var path2 = Pathname.$new('qux');
-      expect(path1['$+'](path2)['$to_path']()).toBe('/foo/bar/qux');
+      expect(path1['$+'](path2)['$to_path']()).to.equal('/foo/bar/qux');
     });
   });
 
   describe('When nodejs module is loaded', function() {
-    it('should resolve Node.js specific implementations', function() {
+    it('should register Node.js specific implementations', function() {
       Opal.load('nodejs');
       var Dir = Opal.const_get_relative([], 'Dir');
       var currentDir = Dir['$pwd']();
-      expect(currentDir).toBe(process.cwd());
+      expect(currentDir).to.equal(process.cwd());
     });
   });
 });
