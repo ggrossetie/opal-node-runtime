9e4226b812200d0dfaacbe537cc81dd23f146268
Guillaume Grossetie <ggrossetie@gmail.com>
2018-09-04 13:33:34 +0200

    Save and restore Function methods

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,21 +1,31 @@
 var Opal = require('../src/index').Opal;
 
-describe('Opal node runtime', function () {
+describe('Opal Node Runtime', function () {
 
   describe('When loaded', function() {
-    it('Opal should not be null', function() {
+    it('should be instantiated', function() {
       expect(Opal).not.toBe(null);
     });
 
-    it('Pathname methods should be available', function() {
+    it('should preserve Function methods', function() {
+      expect(Function.call).toBeDefined();
+      expect(Function.apply).toBeDefined();
+      expect(Function.bind).toBeDefined();
+    });
+  });
+
+  describe('When pathname module is loaded', function() {
+    it('should resolve Pathname methods', function() {
       Opal.load('pathname');
       var Pathname = Opal.const_get_relative([], 'Pathname');
       var path1 = Pathname.$new('/foo/bar');
       var path2 = Pathname.$new('qux');
       expect(path1['$+'](path2)['$to_path']()).toBe('/foo/bar/qux');
     });
+  });
 
-    it('Node.js specific implementations should be available', function() {
+  describe('When nodejs module is loaded', function() {
+    it('should resolve Node.js specific implementations', function() {
       Opal.load('nodejs');
       var Dir = Opal.const_get_relative([], 'Dir');
       var currentDir = Dir['$pwd']();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 /* global Opal */
+var functionCall = Function.call;
+var functionApply = Function.apply;
+var functionBind = Function.bind;
 require('./opal.js');
+// restore Function methods (see https://github.com/opal/opal/issues/1846)
+Function.call = functionCall;
+Function.apply = functionApply;
+Function.bind = functionBind;
 require('./nodejs.js');
 require('./pathname.js');
 require('./base64.js');
