3718be0963c985c9db32a51c75474adbe34baa18
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-16 16:42:35 +0200

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /* global Opal */
-require('./opal.js');
-require('./nodejs.js');
-require('./pathname.js');
-require('./base64.js');
-require('./open-uri.js');
-require('./stringio.js');
+require('./opal.js')
+require('./nodejs.js')
+require('./pathname.js')
+require('./base64.js')
+require('./open-uri.js')
+require('./stringio.js')
 
-module.exports.Opal = Opal;
+module.exports.Opal = Opal
