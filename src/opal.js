67e7dedc8b87155cd65bb844001ef1f149728937
Guillaume Grossetie <ggrossetie@gmail.com>
2020-06-24 13:41:11 +0200

    Update to Opal 1.0.0.dev (df758db7)

--- src/opal.js
+++ src/opal.js
@@ -2040,6 +2040,8 @@
     var i, keys = hash.$$keys, length = keys.length, value;
 
     if (key.$$is_string) {
+      if (typeof key !== "string") key = key.valueOf();
+
       if (!$has_own.call(hash.$$smap, key)) {
         return;
       }
@@ -2319,7 +2321,7 @@
   // Combine multiple regexp parts together
   Opal.regexp = function(parts, flags) {
     var part;
-    var ignoreCase = flags && flags.indexOf('i') >= 0;
+    var ignoreCase = typeof flags !== 'undefined' && flags && flags.indexOf('i') >= 0;
 
     for (var i = 0, ii = parts.length; i < ii; i++) {
       part = parts[i];
